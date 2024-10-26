2afc74b4a49b55b4ce451a619ab5282f61f1d9f1
Aum <aumsoni2002@gmail.com>
2024-10-25 23:02:14 -0400

    Feature in Progress: Sorting (Table Component)

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,16 @@
+import { Fragment } from "react";
+
 function Table({ data, config, keyFn }) {
   const renderedHeaders = config.map((column) => {
+    if (column.header) {
+      // Fragment is a react component which we can make use of as an HTML element that does not get displayed on the browser.
+      // we are making use of this component's tag because, we already have put th element inside the 'column' object in TablePage component.
+      // so we cannot make use of another html element here, the table structure will show the error: header: () => <th className="bg-red-500">Score</th>
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
+    }
     return <th key={column.label}>{column.label}</th>;
   });
+
   const renderedRows = data.map((rowData) => {
     const renderedCells = config.map((column) => {
       return (
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -6,7 +6,7 @@ function TablePage() {
     { name: "Apple", color: "bg-red-500", score: 3 },
     { name: "Banana", color: "bg-yellow-500", score: 1 },
     { name: "Lime", color: "bg-green-500", score: 4 },
-  ];
+  ];   
 
   const config = [
     { label: "Name", render: (fruit) => fruit.name },
@@ -14,7 +14,11 @@ function TablePage() {
       label: "Color",
       render: (fruit) => <div className={`p-2 m-3 ${fruit.color}`}></div>,
     },
-    { label: "Score", render: (fruit) => fruit.score },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      header: () => <th className="bg-red-500">Score</th>,
+    },
   ];
 
   const keyFn = (fruit) => {
